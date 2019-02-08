0bb57fccb610bfd1060af39632e235a1a1ac5c07
KirtiJain121 <47408935+KirtiJain121@users.noreply.github.com>
2019-02-08 16:56:15 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,37 @@
+$( ".bd" ).addClass( "background1" );
+
+ 
+            jQuery(document).ready(function($) {
+             $('.owl-carousel').owlCarousel({
+    items:1,
+    margin:10,
+    autoHeight:true,
+     autoplay:true,
+     loop:true,
+      autoplayTimeout:2500,
+    autoplayHoverPause:true
+
+});
+        
+            });
+
+             jQuery(document).ready(function($) {
+
+              var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+ });
+ 
